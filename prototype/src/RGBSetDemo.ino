5344de8b668789dbbbc63f2f156f9d7fc37abedb
tangentmonger <tangentmonger@users.noreply.github.com>
2017-11-28 01:46:19 +0000

    Add top and bottom bars, and white separators

--- prototype/src/RGBSetDemo.ino
+++ prototype/src/RGBSetDemo.ino
@@ -14,6 +14,9 @@ const int TOP_N = NUM_LEDS - TOP_START;
 const int SIDE_N = LEFT_N;
 const int ROWS_N = SIDE_N + 2;
 
+const int TOP_PAGE_SHOW = 3;
+const int PAGE_N = 12;
+
 CRGBArray<NUM_LEDS> leds;
 
 // namespace this to let Arduino see it
@@ -28,8 +31,12 @@ phone::Row rows[ROWS_N];
 
 const int hue_variance = 40;
 const int brightness = 180;
+const int required_hue_difference = 50;
 
-uint8_t * hue[ROWS_N * 2];
+uint8_t previous_hue = 0;
+
+uint8_t * hue[TOP_PAGE_SHOW + PAGE_N * 3]; // enough space to build up a full screenful of pages before it's needed
+uint8_t * sat[TOP_PAGE_SHOW + PAGE_N * 3]; // enough space to build up a full screenful of pages before it's needed
 
 void setup() {
 
@@ -63,24 +70,51 @@ void setup() {
 // s-curve the accellection, maybe overshoot and snap back
 // blend the variation, use a variation delta instead?
 
-// may need to do something differnet to the top/bottom bars, too much colour change at once
-
+// scroll speed linger time: linger on 'posts' they like (similar to heart colour)
 
 void loop(){
 
     // choose a new base hue
-    uint8_t next_hue = random(256 - hue_variance);
-
-    // put that hue (+ variance) into lower half of hues
-    for (int i=0; i<ROWS_N; i++) {
-        hue[ROWS_N + i] = next_hue + random(hue_variance);
+    uint8_t next_hue;
+    bool ok = false;
+    while(!ok) {
+        next_hue = random(256 - hue_variance);
+        if(next_hue > previous_hue && next_hue - previous_hue > required_hue_difference) ok = true;
+        if(next_hue < previous_hue && previous_hue - next_hue > required_hue_difference) ok = true;
     }
 
-    // scroll: put hues onto rows by moving pointer down and applying hues with short delays
-    for (uint8_t top=0; top<=ROWS_N; top++) {
-        for(int i=0; i<ROWS_N; i++) {
-            for(int led=0; led<rows[i].leds; led++) {
-                leds[rows[i].positions[led]] = CHSV(hue[top+i], 255, brightness);
+
+    // put that hue (+ variance) into third full page
+    for (int i=0; i<PAGE_N; i++) {
+        hue[PAGE_N + PAGE_N + TOP_PAGE_SHOW + i] = next_hue + random(hue_variance);
+    }
+
+    // add flapbook borders by setting hue and saturation
+    sat[PAGE_N+PAGE_N+TOP_PAGE_SHOW] = 0;
+    //hue[PAGE_N+PAGE_N+TOP_PAGE_SHOW] = 160; // blue
+    sat[PAGE_N+PAGE_N+TOP_PAGE_SHOW + 1] = 0;
+    sat[PAGE_N+PAGE_N+TOP_PAGE_SHOW + 2] = 0;
+    //hue[PAGE_N+PAGE_N+TOP_PAGE_SHOW + 2] = 160; //blue
+    //sat[PAGE_N+PAGE_N+TOP_PAGE_SHOW + 3] = 200;
+    //hue[PAGE_N+PAGE_N+TOP_PAGE_SHOW + 3] = 160; //blue
+    
+
+    //sat[PAGE_N+PAGE_N+TOP_PAGE_SHOW + 4] = 0; //white
+    //sat[PAGE_N+PAGE_N+TOP_PAGE_SHOW + 5] = 0; //white
+    for (int i=3; i< PAGE_N; i++) {
+        sat[PAGE_N + PAGE_N + TOP_PAGE_SHOW + i] = 255;
+    }
+
+    // scroll: put pageful of hues onto rows by moving pointer down and applying hues with short delays
+    for (uint8_t top=0; top<=PAGE_N; top++) { // for each row of the page...
+        for(uint8_t i=0; i<ROWS_N; i++) { // for each row of the display ...
+            for(int led=0; led<rows[i].leds; led++) { // for each LED in the row ...
+                if(i<3 || i>17) {
+                    leds[rows[i].positions[led]] = CHSV(160, 255, brightness / 4 * 3); // set the LED's colour (flapbook border)
+                    // leds[rows[i].positions[led]] = CHSV(145, 255, brightness); // set the LED's colour (twitflange border)
+                } else {
+                    leds[rows[i].positions[led]] = CHSV(hue[top+i], sat[top+i], brightness); // set the LED's colour
+                }
             }
 
         }
@@ -92,15 +126,16 @@ void loop(){
     }
 
 
-    // swap lower half of hues into upper half and reset pointer
-    for(int i=0; i<ROWS_N; i++) {
-        hue[i] = hue[ROWS_N + i];
+    // shuffle second page of hues and saturations into first page
+    for(int i=0; i<PAGE_N + PAGE_N + TOP_PAGE_SHOW; i++) {
+        hue[i] = hue[PAGE_N + i];
+        sat[i] = sat[PAGE_N + i];
     }
 
 
     // long delay
 
-
+    previous_hue = next_hue;
      FastLED.delay(1000);
 
 
